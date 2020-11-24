b0d1d8f0cc67d4bf4b376af147234ea952a7997b
robertoandres24 <robertoandres24@gmail.com>
2020-11-24 19:10:54 -0300

    get todos length in Navbar, through context api

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,28 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 
 import { Link } from "react-router-dom";
+import { TodoContext } from '../context/TodoContext';
 
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar bg-light navbar-expand-lg navbar-light">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-            <Link to="/" className="nav-link">Todos</Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/create" className="nav-link">Create Todo</Link>
-          </li>
-        </ul>
-        <ul className="navbar-nav">
-          <li className="navbar-item">
-            <a href="#" className="nav-link"> Todos Count: 4</a>
-          </li>
-        </ul>
-      </nav>
-    )
-  }
+const NavBar = () => {
+  const { todos } = useContext(TodoContext)
+  return (
+    <nav className="navbar bg-light navbar-expand-lg navbar-light">
+      <ul className="navbar-nav mr-auto">
+        <li className="navbar-item">
+          <Link to="/" className="nav-link">Todos</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/create" className="nav-link">Create Todo</Link>
+        </li>
+      </ul>
+      <ul className="navbar-nav">
+        <li className="navbar-item">
+          <a href="#" className="nav-link"> Todos Count: {todos.length}</a>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default NavBar
